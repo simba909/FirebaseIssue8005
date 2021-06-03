2d69d6d7d5295ce191e74f9bf35c09d4358ff947
Simon Jarbrant <simonjarbrant@me.com>
2021-06-03 10:16:50 +0200

    Initial Commit

---
+++ FirebaseIssue8005/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseIssue8005
+//
+//  Created by Simon Jarbrant on 2021-06-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseIssue8005/FirebaseIssue8005App.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseIssue8005App.swift
+//  FirebaseIssue8005
+//
+//  Created by Simon Jarbrant on 2021-06-03.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseIssue8005App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
